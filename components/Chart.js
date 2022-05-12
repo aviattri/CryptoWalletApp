f62eed30f751b6e41b28be7e75055efd37cb2e6d
aviattri <avirajattri8@gmail.com>
2022-05-12 17:45:01 +1000

    fix dependencies

--- components/Chart.js
+++ components/Chart.js
@@ -1,5 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import {
+  ChartDot,
+  ChartPath,
+  ChartPathProvider,
+  monotoneCubicInterpolation,
+} from "@rainbow-me/animated-charts";
 
 const Chart = ({ containerStyle, chartPrices }) => {
   return (
